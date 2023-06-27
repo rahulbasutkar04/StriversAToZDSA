11ea220c1d807b43929839a929a6ed268e91579e
Rahul Basutkar <115400916+rahulbasutkar04@users.noreply.github.com>
2023-06-27 23:12:23 +0530

    Create third.java

---
+++ LogicBuilding/third.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class third {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();//input
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+output-
+5
+  
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+  
