ecf02f3f8cfde7b227f8897e390966f7cfd50f74
Rahul Basutkar <115400916+rahulbasutkar04@users.noreply.github.com>
2023-06-28 22:51:04 +0530

    nine.java

--- LogicBuilding/nine.java
+++ LogicBuilding/nine.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class first {
+public class nine {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();//input
