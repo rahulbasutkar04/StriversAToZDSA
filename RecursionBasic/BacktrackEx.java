c903d51855bdd3aaebfabd4bcf4996f767b3004f
Rahul Basutkar <115400916+rahulbasutkar04@users.noreply.github.com>
2023-08-05 20:16:33 +0530

    Update BacktrackEx.java

--- RecursionBasic/BacktrackEx.java
+++ RecursionBasic/BacktrackEx.java
@@ -1,4 +1,4 @@
-//print 1 to n using backtrack approach
+//print 1 to n using backtrack approach 1 to n
 import  java.util.*;
 public class BacktrackEx {
     static  void backtrackex(int i,int n)
@@ -13,6 +13,26 @@ public class BacktrackEx {
      Scanner sc=new Scanner(System.in);
      int n=sc.nextInt();
      backtrackex(n,n);
+
+        System.out.println("End");
+    }
+}
+
+//print n to 1 using backtrack approach
+import  java.util.*;
+public class BacktrackEx {
+    static  void backtrackex(int i,int n)
+    {
+        if(i>n)//changed
+            return;
+
+        backtrackex(i+1,n);//chnaged
+        System.out.println(i+" ");//remeber in backtrack the print stmt is after the funtion call
+    }
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     backtrackex(1,n);//changed
 
         System.out.println("End");
     }
