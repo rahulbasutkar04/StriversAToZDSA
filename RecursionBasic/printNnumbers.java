f45d521a9ebd49bf24c102a316d54dcf91f8b2d7
Rahul Basutkar <115400916+rahulbasutkar04@users.noreply.github.com>
2023-08-05 20:03:00 +0530

    Update printNnumbers.java

--- RecursionBasic/printNnumbers.java
+++ RecursionBasic/printNnumbers.java
@@ -20,3 +20,27 @@ public class printNumbers {
 
     }
 }
+// to print reverse numbers like 5,4,3,2,1
+
+//printt numbers linearly 1,2,3,4....n
+import java.util.*;
+public class printNumbers {
+    static  void pnum(int i,int n)
+    {
+        if(i<1) {//changed
+            System.out.println();
+            return;
+        }
+
+        System.out.print(i+" ");
+        pnum(i-1,n);//changed
+    }
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pnum(n,n);//changed
+        System.out.println("End");
+
+    }
+}
