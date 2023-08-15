fe2e6e3fc1dd21d9ec4fbc6fc98ac2e139dc3709
Rahul Basutkar <115400916+rahulbasutkar04@users.noreply.github.com>
2023-08-15 12:15:47 +0530

    Create RecursiveBubble.java

---
+++ SortingTechniques/RecursiveBubble.java
@@ -0,0 +1,34 @@
+public class recursiveBubble{
+    public static void bubbleSort(int [] arr,int n)
+    {
+        if(n==1) return;
+        for(int i=0;i<=n-2;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+        bubbleSort(arr,n-1);
+
+    }
+    public static void main(String[] args) {
+        int [] arr={10,1,56,21,6,4,9,8,32,45,78};
+        int n=arr.length;
+        System.out.println("Elements after sort:");
+        bubbleSort(arr,n);
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+"\t");
+        }
+    }
+}
+
+
+output
+  Elements after sort:
+1	4	6	8	9	10	21	32	45	56	78
+
+  //in recursive approach if u figured out we used only one loop instead of two.
