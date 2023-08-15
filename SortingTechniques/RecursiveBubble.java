b6740947028c6c279cd1ca2f642c1d643767133a
Rahul Basutkar <115400916+rahulbasutkar04@users.noreply.github.com>
2023-08-15 12:17:30 +0530

    Update RecursiveBubble.java

--- SortingTechniques/RecursiveBubble.java
+++ SortingTechniques/RecursiveBubble.java
@@ -2,6 +2,7 @@ public class recursiveBubble{
     public static void bubbleSort(int [] arr,int n)
     {
         if(n==1) return;
+        int flag=0;
         for(int i=0;i<=n-2;i++)
         {
             if(arr[i]>arr[i+1])
@@ -9,8 +10,10 @@ public class recursiveBubble{
                 int temp=arr[i];
                 arr[i]=arr[i+1];
                 arr[i+1]=temp;
+                flag=1;
             }
         }
+        if(flag==0)return;
         bubbleSort(arr,n-1);
 
     }
