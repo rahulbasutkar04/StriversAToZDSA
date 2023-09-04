0585ce1b79c64d0f89d866fef032abbb35b97f34
Rahul Basutkar <115400916+rahulbasutkar04@users.noreply.github.com>
2023-09-04 23:34:04 +0530

    Create ThreeSum.java

---
+++ Arrays/ThreeSum.java
@@ -0,0 +1,46 @@
+   import java.util.*;
+
+public class ThreeSum{
+    public static List<List<Integer>> triplet(int n, int[] arr) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            //remove duplicates:
+            if (i != 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+            //moving 2 pointers:
+            int j = i + 1;
+            int k = n - 1;
+            while (j<k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    //skip the duplicates:
+                    while (j < k && arr[j] == arr[j - 1]) j++;
+                    while (j < k && arr[k] == arr[k + 1]) k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { -1, 0, 1, 2, -1, -4};
+        int n = arr.length;
+        List<List<Integer>> ans = triplet(n, arr);
+        System.out.println(ans);
+    }
+}
+
+//output  tc=O(N^2)
+[[-1, -1, 2], [-1, 0, 1]]
