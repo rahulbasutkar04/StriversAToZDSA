fcd94cacc6d1a2913487e53b7fd04f99e0fa9da0
Rahul Basutkar <115400916+rahulbasutkar04@users.noreply.github.com>
2023-09-14 19:14:01 +0530

    Update misAndRepeated.java

--- Arrays/misAndRepeated.java
+++ Arrays/misAndRepeated.java
@@ -28,3 +28,49 @@ public class missingandrepe {
 }
 //ouput  tc=O(N^2)
 Repeated:5 missiong:8
+//optimal
+    //find missinf and repeated number from the gien n numbers
+//optimal approach
+public class missingandrepe {
+    public static void main(String[] args) {
+        int [] arr={3, 1, 2, 5, 4, 6, 7, 5};
+
+        int n=arr.length;
+        //calculate sum of n
+        int sn=n*(n+1)/2;
+
+        //calculat sum of square of n
+        int s2n=(n*(n+1)*(2*n+1))/6;
+
+        //calculate sum of elements in arr
+        int s=0;
+        int s2=0;
+        for(int i=0;i<n;i++)
+        {
+            s+=arr[i];
+
+            s2+=(arr[i]*arr[i]);
+        }
+        //now we know that x is repeating element and y is missing
+        //to calculate x-y
+        //sum of arr element - sum of n elements
+        //to calculate x^2-y^2
+        //sum of squre of arr elements - sum of square of n elements
+        int val1=s-sn;//x-y
+        int val2=s2-s2n;//x^2-y^2
+        val2=val2/val1;//x+y
+        //w k t (x+y)+(x-y)=2x
+
+        int sum=val1+val2;
+        int x=sum/2;
+        int y=x-val1;
+
+        System.out.println("Repeating number is:"+x+" "+"Missing Number is:"+y);
+
+
+
+
+
+    }
+}
+//tc=O(N)
