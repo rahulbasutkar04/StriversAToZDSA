7ae375bed701cdfd7afde753355f272747d87e0e
Rahul Basutkar <115400916+rahulbasutkar04@users.noreply.github.com>
2023-10-26 05:10:18 +0530

    Create maxDepth.java

---
+++ Strings/maxDepth.java
@@ -0,0 +1,33 @@
+public static int maxDepth(String S){
+int current_max = 0; 
+    int max = 0; 
+    int n = S.length(); 
+ 
+    
+    for (int i = 0; i < n; i++) 
+    { 
+        if (S[i] == '(') 
+        { 
+            current_max++; 
+            if (current_max > max) 
+                max = current_max; 
+        } 
+        else if (S[i] == ')') 
+        { 
+            if (current_max > 0) 
+                current_max--; 
+            else
+                return -1; 
+        } 
+    } 
+ 
+    
+    if (current_max != 0) 
+        return -1; 
+ 
+    return max; 
+} 
+
+}
+
+//Time Complexity->O(N)
