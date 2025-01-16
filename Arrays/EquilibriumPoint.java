0f782caa2afb84a22c7fe617c3947f909dcf9bc6
Rahul Basutkar <115400916+rahulbasutkar04@users.noreply.github.com>
2025-01-16 23:32:36 +0530

    Create EquilibriumPoint.java

---
+++ Arrays/EquilibriumPoint.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(
+                    token)); // Use Integer.parseInt to parse int values
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling equilibriumPoint() function
+            System.out.println(obj.findEquilibrium(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find equilibrium point in the array.
+    public static int findEquilibrium(int arr[]) {
+        // code here
+        
+        int n=arr.length;
+        
+        int rightSum=0;
+        int leftSum=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            rightSum+=arr[i];
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            rightSum-=arr[i];
+            
+            if(rightSum==leftSum) return i;
+            
+            leftSum+=arr[i];
+        }
+        
+        return -1;
+    }
+}
